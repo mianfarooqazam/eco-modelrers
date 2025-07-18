1d7859d235fb0d37f6ab51c31e2f6a376c8ba507
mianfarooqazam <farooqazammian@gmail.com>
2025-07-18 11:58:24 +0500

    Revert "Text animation added"
    This reverts commit cbbd89b02e1f71e3e61cc2fe847298f1f1937203.

--- src/app/pages/home.js
+++ src/app/pages/home.js
@@ -1,18 +1,8 @@
-import TypewriterTitle from "../components/TypewriterTitle";
-
 export default function HomeSection() {
   return (
-    <section id="home" className="h-screen flex flex-col md:flex-row items-center justify-center px-[10px] mx-4 md:mx-16 rounded-2xl bg-purple-600">
-      {/* Left: Typewriter text, 50% width */}
-      <div className="flex-1 md:basis-1\/2 flex items-end justify-center w-full h-full pb-12">
-        <TypewriterTitle />
-      </div>
-      {/* Right: Image placeholder, 50% width */}
-      <div className="flex-1 md:basis-1\/2 flex items-center justify-center w-full h-full">
-        <div className="w-72 h-72 bg-white/30 rounded-xl flex items-center justify-center text-white text-lg font-semibold">
-          Image Placeholder
-        </div>
-      </div>
+    <section id="home"  className="h-screen flex flex-col items-center justify-center px-[10px] mx-4 md:mx-16 rounded-2xl  bg-purple-600">
+      <h1 className="text-4xl font-bold mb-4">Home</h1>
+      <p className="text-lg">Welcome to our website! We provide innovative solutions to help your business grow.</p>
     </section>
   );
 } 
